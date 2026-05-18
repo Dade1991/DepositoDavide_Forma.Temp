82d223041adf5391fd7857e8aab42ea32a61ff73
Dade1991 <dade91@msn.com>
2026-05-18 16:05:51 +0200

    Afternoon exercise

---
+++ 18 May 2026/Esercizio_Pomeriggio/main12.js
@@ -0,0 +1,91 @@
+const prompt = require("prompt-sync")()
+
+// Inizializzo Array vuoto
+
+let numbers = []
+
+// Inserimento numeri da utente (mentre è vero, ed è sempre vero fino a che non si digita 0, rimane in ascolto di input utenti)
+
+while (true) {
+  let userInput = prompt(
+    "Insert here a number (digit `0` to terminate program): ",
+  )
+  let number = Number(userInput)
+
+  // Controllo dell'errore (se non è un numero, gestisce l'errore tornando un messaggio per l'utente e non bloccca il processo di while)
+
+  if (Number.isNaN(number)) {
+    console.log("Input not valid, insert a valid number.")
+
+    continue
+  }
+
+  // Blocca il processo di while e chiude correttamente il programma
+
+  if (number === 0) {
+    console.log("Program closed.")
+
+    break
+  }
+
+  // Aggiunge il numero appena inserito nell'array inizializzato all'inizio codice
+
+  numbers.push(number)
+
+  // Stampa numeri inseriti all'interno dell'array fino ad ora
+
+  console.log(numbers)
+}
+
+// Inizializzo Arrays vuoti per numeri pari, dispari ed inizializzo varibile per conteggio somma tot numeri dell'array principale (numbers)
+
+let even = []
+let odd = []
+let sum = 0
+
+// Gestisco gli eventuali errori se l'array fosse vuoto: restitusco null
+
+let min = null
+let max = null
+
+for (number of numbers) {
+  // Qui sommo tutti i numeri che aggiungo mano a mano in maniera "automatica"
+
+  sum += number
+
+  // Poi, controllo che i numeri siano o pari o dispari e gli aggiungo ad un array o all'altro.
+
+  if (number % 2 === 0) {
+    even.push(number)
+  } else {
+    odd.push(number)
+  }
+
+  // Se non c'è ancora un valore, assegno il primo valore che trovo.
+  // Oppure, assegno il numero appena inserito come min se questo stesso è il più piccolo del minimo attuale tra quelli gia presenti. (Quello che tende di più verso valore basso)
+
+  if (min === null || number < min) {
+    min = number
+  }
+
+  // Se non c'è ancora un valore, assegno il primo valore che trovo.
+  // Oppure, assegno il numero appena inserito come max se questo stesso è il più grande del massimo attuale tra quelli gia presenti. (Quello che tende di più verso valore alto)
+
+  if (max === null || number > max) {
+    max = number
+  }
+}
+
+// Stampe totali degli arrays & varibili richieste
+
+console.log("Collected tot numbers: " + numbers)
+console.log("Collected even numbers: " + even)
+console.log("Collected odd numbers: " + odd)
+console.log("Total sum is: " + sum)
+
+if (numbers.length > 0) {
+  console.log("Minimum number is: " + min)
+  console.log("Maximum number is: " + max)
+} else {
+  console.log("No number inserted.")
+}
---
+++ 18 May 2026/Pomerggio/main11.js
@@ -0,0 +1,83 @@
+// Arrays
+
+let numbers = [1, 2, 3]
+
+// Accesso agli elementi
+
+console.log(numbers[0]) // 1
+
+// Aggiunta elementi
+
+numbers.push(4) // aggiunge in fondo -> [1,2,3,4]
+
+console.log(numbers)
+
+numbers.unshift(0) // aggiunge all'inizio -> [0,1,2,3,4]
+
+console.log(numbers)
+
+// Rimozione elementi
+
+numbers.pop() // rimuove ultimo -> [0,1,2,3]
+
+console.log(numbers)
+
+numbers.shift() // rimuove primo -> [1,2,3]
+
+console.log(numbers)
+
+// Iterazione
+
+numbers.forEach((n) => console.log("I numeri stampati forEach n sono: " + n))
+
+// Trasformazione
+
+let double = numbers.map((n) => n * 2) // [2,4,6]
+
+console.log("Il risultato doppio per ciascun valore (indicizzato) è: " + double)
+
+// Filtro
+
+let majorOne = numbers.filter((n) => n > 1) // [2,3]
+
+console.log("Filtra i numeri desiderati superiori al valore 1: " + majorOne)
+
+// Ricerca
+
+let found = numbers.find((n) => n === 2) // 2
+
+console.log("Ricerca il numero indicato nella parametro: " + found)
+
+// Lunghezza
+
+console.log("La lunghezza tot dell'array è: " + numbers.length) // 3
+
+// Array nei cicli - for-of itera sui valori di strutture iterabili (FOR-OF)
+
+let numbers2 = [10, 20, 30]
+
+for (let value of numbers2) {
+  console.log("Valore in array: " + value) // 10, 20, 30
+}
+
+let stringEx = ["Ciao", "Sono ", "Dave", 101]
+
+for (let stringResult of stringEx) {
+  console.log("Valore in array: " + stringResult) // Ciao Sono Dave
+}
+
+// Esempio più esplicativo
+
+let word = "Davide"
+
+for (let singleWord of word) {
+  console.log(singleWord) // D, a, v, i, d, e
+}
+
+// Array nei cicli - for-in itera sulle proprietà di un oggetto. (FOR-IN)
+
+let person = { name: "Mario", age: 30 }
+
+for (let key in person) {
+  console.log("Valore in array: " + key, person[key])
+}
