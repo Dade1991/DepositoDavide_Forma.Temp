d623582a11a2c1ce999bdb63262e4b45448278f3
Dade1991 <dade91@msn.com>
2026-05-19 16:32:52 +0200

    Added a new exercise to practice with

---
+++ 19 May 2026/Esercizio_Pomeriggio/mainEX.js
@@ -0,0 +1,58 @@
+// 1. Rubrica semplice
+// Crea un array di contatti, dove ogni contatto ha name, phone, email.
+// Scrivi funzioni per:
+
+// - stampare tutti i contatti;
+
+// - cercare un contatto per nome;
+
+// - aggiungere un nuovo contatto;
+
+// - contare quanti contatti ci sono in totale.
+
+let contacts = [
+  {
+    name: "Clara",
+    phone: 33933322266,
+    email: "claretta@amour.com",
+  },
+  {
+    name: "Rik",
+    phone: 33845454571,
+    email: "rik@gmail.com",
+  },
+  {
+    name: "Tiz",
+    phone: 32755599900,
+    email: "tiz@google.com",
+  },
+  {
+    name: "Mirko",
+    phone: 33500334466,
+    email: "mirko@gmail.com",
+  },
+]
+
+function showAllContacts() {
+  let allContactDetails = []
+
+  for (let i = 0; i < contacts.length; i++) {
+    let contact = contacts[i]
+    console.log(contact)
+  }
+}
+
+function findByName(nameToFind) {
+  for (let i = 0; i < contacts.length; i++) {
+    let contact = contacts[i]
+    if (contact.name === nameToFind) {
+      return contact
+    }
+  }
+  return "Invalid name, please try with another name."
+}
+
+function addNewContact(contactDetails) {}
+
+showAllContacts()
+console.log(findByName("Tiz"))
