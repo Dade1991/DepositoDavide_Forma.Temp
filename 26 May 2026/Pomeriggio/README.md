19cd0234295a3553f435b27d426c6a8a9cf988d9
Dade1991 <dade91@msn.com>
2026-05-26 16:01:42 +0200

    Added other file to read with Node.js

---
+++ 26 May 2026/Pomeriggio/README.md
@@ -0,0 +1,1 @@
+Ciao ancora Davide! Questo altro messaggio arriva da un file in formato .md letto da Node.js
--- 26 May 2026/Pomeriggio/server.js
+++ 26 May 2026/Pomeriggio/server.js
@@ -1,5 +1,12 @@
+// Da qui avvio tutto
+
+// Inzializzo una variabile per l'import del modulo con la funzione showMessage
+
 const { showMessage } = require("./messages")
+
+// Qui evoco la funzione showMessage indicando quali dei file dovrà mostrare con i relativi paths
 
 showMessage("./message.txt")
 showMessage("./Index.html")
 showMessage("./config.json")
+showMessage("./README.md")
