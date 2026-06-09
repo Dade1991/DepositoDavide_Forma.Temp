50026a9c29955b8fef65de35d67415d9c943d573
Dade1991 <dade91@msn.com>
2026-06-09 12:36:49 +0200

    Morning exercises

--- 09 Jun 2026/my-angular-app/src/app/app.ts
+++ 09 Jun 2026/my-angular-app/src/app/app.ts
@@ -1,10 +1,13 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { BaseComponent } from "./features/base-component/base-component";
+import { TitleComponent } from "./features/title-component/title-component";
+import { ParagraphComponent } from "./features/paragraph-component/paragraph-component";
+import { ImageComponent } from "./features/image-component/image-component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, BaseComponent],
+  imports: [RouterOutlet, BaseComponent, TitleComponent, ParagraphComponent, ImageComponent],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
